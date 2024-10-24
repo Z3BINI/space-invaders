2c3db06df31ede3effda23d4c5752f8851eaeac0
Eusébio Alves <eusebio.a95@hotmail.com>
2024-10-24 10:17:31 +0100

    Player movement with input
    KeyboardHandler's onKeyEvent method will listen for arrow key left/right to set the velocity.x to either - maxSpeed for left, maxSpeed for right or 0 for stop.
    
    That value will be applied to the player's position in update. Also, clamping the position within the play screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
 import 'package:space_invaders/components/player.dart';
 
 
-class SpaceInvaders extends FlameGame {
+class SpaceInvaders extends FlameGame with HasKeyboardHandlerComponents {
   late final Player player; //  Disallow null & reference alteration
 
   @override
