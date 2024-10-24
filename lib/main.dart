1bf62b90d69e0947012525959eee5642ace066a7
Eusébio Alves <eusebio.a95@hotmail.com>
2024-10-24 09:28:49 +0100

    Instantiate player and add to game world

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:async';
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
+import 'package:space_invaders/components/player.dart';
 
 
 class SpaceInvaders extends FlameGame {
+  late final Player player; //  Disallow null & reference alteration
+
+  @override
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
+
+    player = Player();
+
+    add(player);
+  }
 
 }
 
