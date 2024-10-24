2d1d1a504f5a07e6c0effc47e8fe08a32a7ec195
Eusébio Alves <eusebio.a95@hotmail.com>
2024-10-24 09:22:33 +0100

    Paint player sprite on screen

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,6 +1,20 @@
+import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:space_invaders/main.dart';
 
 class Player extends SpriteComponent with HasGameRef<SpaceInvaders> {
+  
+  Player () : super ( // Initialize parent's required parameters.
+    size: Vector2.all(32),
+    anchor: Anchor.center,
+  );
+  
+  @override
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
 
+    sprite = await gameRef.loadSprite('player.png');
+
+    position = Vector2(gameRef.size.x / 2, gameRef.size.y - size.y / 2); // Bottom center of screen
+  }
 }
