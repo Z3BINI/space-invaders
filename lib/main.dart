e6e10507de45f02c8c175457e061044691b1c9c1
Eusébio Alves <eusebio.a95@hotmail.com>
2024-10-27 18:21:32 +0000

    Instructions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:space_invaders/components/space_invaders.dart';
@@ -6,34 +7,44 @@ void main() {
   runApp(GameApp());
 }
 
-
 class GameApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     final spaceInvadersGame = SpaceInvaders();
 
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
+        body: Column(
           children: [
-            Center(
-              child: SizedBox(
-                width: SpaceInvaders.gameResolution.x,
-                height: SpaceInvaders.gameResolution.y,
-                child: GameWidget(
-                  game: spaceInvadersGame,
-                  overlayBuilderMap: {
-                    'MainMenu': (context, SpaceInvaders spaceInvadersGame) {
-                      return MainMenuOverlay(
-                        onStartPressed: () {
-                          spaceInvadersGame.startGame(); // Start the game
-                          spaceInvadersGame.overlays.remove('MainMenu'); // Hide menu overlay
-                        },
-                      );
+            Expanded(
+              child: Center(
+                child: SizedBox(
+                  width: SpaceInvaders.gameResolution.x,
+                  height: SpaceInvaders.gameResolution.y,
+                  child: GameWidget(
+                    game: spaceInvadersGame,
+                    overlayBuilderMap: {
+                      'MainMenu': (context, SpaceInvaders spaceInvadersGame) {
+                        return MainMenuOverlay(
+                          onStartPressed: () {
+                            spaceInvadersGame.startGame(); // Start the game
+                            spaceInvadersGame.overlays.remove('MainMenu'); // Hide menu overlay
+                          },
+                        );
+                      },
                     },
-                  },
-                  initialActiveOverlays: const ['MainMenu'], // Show menu first
+                    initialActiveOverlays: const ['MainMenu'], // Show menu first
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'ARROWS: move | SPACE: shoot | G: style! 😎',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
                 ),
               ),
             ),
@@ -43,7 +54,6 @@ class GameApp extends StatelessWidget {
       ),
     );
   }
-  
 }
 
 class MainMenuOverlay extends StatelessWidget {
