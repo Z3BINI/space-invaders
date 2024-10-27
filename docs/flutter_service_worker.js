95b71486af778dee1b692aa512b492a93b8922b2
Eusébio Alves <eusebio.a95@hotmail.com>
2024-10-27 18:33:38 +0000

    web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1c1c96ab5e3f5436e858181a64874bbe
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "882731b760fb4b62aa4bb20d0c54d9bf",
+"flutter_bootstrap.js": "6e72e63c665879656d58df0854fa1763",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
